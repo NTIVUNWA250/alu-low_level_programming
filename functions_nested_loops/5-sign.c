7ebe00898dde205e2e2193457c34a72be0ddaac1
NTIVUNWA250 <g.ntivunwa@alustudent.com>
2026-09-21 04:27:04 -0700

    a function that prints the sign of a number.

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - checks 1 if it's positive or else -1 or 0
+ * @n: the number to test
+ * Return: int
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+}
